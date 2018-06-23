8f89e15d88524279bfb6e45d1db873ba7a472ed5
Nathan McKinley <nath@nmckinley.com>
2018-06-23 18:58:22 +0000

    a bunch of binary files because i am good at git

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, flash, request, redirect, url_for, render_template
 import os
+import random
 import intel
 
 app = Flask(__name__, static_url_path='/static')
@@ -18,7 +19,9 @@ def upload_file():
             filename = os.path.join('./static', file.filename)
             file.save(filename)
             classes = intel.what_class(filename)
-            return render_template('image.html', classes = classes, filename = file.filename)
+            cl = classes[0][0]
+            ref_img = random.choice([x for x in os.listdir(os.path.join('./static', cl)) if os.path.isfile(x)])
+            return render_template('image.html', classes = classes, filename = file.filename, ref_img=ref_img)
     return render_template('main.html')
 
 if __name__ == '__main__':
