b98e45f0e8f34789e6b097f95421b1fc63e2e665
Nathan McKinley <nath@nmckinley.com>
2018-06-23 18:37:46 +0000

    make a static page that is less bad

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 from flask import Flask, flash, request, redirect, url_for, render_template
 import os
+import intel
 
-app = Flask(__name__, static_url_path='/tmp')
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -14,12 +15,11 @@ def upload_file():
             flash('No selected file')
             return redirect(request.url)
         if file:
-            filename = os.path.join('/tmp', file.filename)
+            filename = os.path.join('./static', file.filename)
             file.save(filename)
-            import intel
             classes = intel.what_class(filename)
             return render_template('image.html', classes = classes, filename = file.filename)
     return render_template('main.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', port=5001, debug=True)
