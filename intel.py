3b0386edb86038afdc03a83341a402830dce451b
Nathan McKinley <nath@nmckinley.com>
2018-06-23 18:32:13 +0000

    ok fine static state is bad

--- intel.py
+++ intel.py
@@ -1,14 +1,14 @@
 import tensorflow as tf
 import sys
 
-label_lines = [line.rstrip() for line in tf.gfile.GFile('/tmp/output_labels.txt')]
-with tf.gfile.FastGFile('/tmp/output_graph.pb', 'rb') as f:
-  graph_def = tf.GraphDef()
-  graph_def.ParseFromString(f.read())
-  _ = tf.import_graph_def(graph_def, name='')
-
 def what_class(image_path):
-  print image_path
+  label_lines = [line.rstrip() for line in tf.gfile.GFile('/tmp/output_labels.txt')]
+  with tf.gfile.FastGFile('/tmp/output_graph.pb', 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    _ = tf.import_graph_def(graph_def, name='')
+
+
   image_data = tf.gfile.FastGFile(image_path, 'rb').read()
 
   with tf.Session() as sess:
